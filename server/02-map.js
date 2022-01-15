7ccb1472e9bcb6bcabb8f859198088bf00738f02
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-15 17:46:11 -0500

    JS estudiando map

---
+++ server/02-map.js
@@ -0,0 +1,14 @@
+const letters = ['a' , 'b' , 'c'];
+
+
+// const newArray = [];
+// for (let index = 0 ; index < letters.length ; index ++) {
+//     let element = letters[index];
+//     newArray.push(element + '++');
+
+// }
+const newArray = letters.map(item => item + '++');
+
+console.log('original', letters);
+console.log('new ' + newArray);
+
