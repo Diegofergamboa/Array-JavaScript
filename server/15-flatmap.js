3259237a50ef3470dc158caccc507d33f6a597b3
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-18 16:12:54 -0500

    Update 15-flatmap.js

--- server/15-flatmap.js
+++ server/15-flatmap.js
@@ -3,3 +3,10 @@ const users = [
     {userID : 2 , NameUser: 'Fernando' , atributes : [28, 'Analista', 'IBM']},
     {userID : 3 , NameUser: 'José' , atributes : [29, 'Data Scientist', 'Oracle']},
 ];
+
+const rtaMapAtributes = users.map(user => user.atributes).flat();
+console.log('Respuesta usando flat y map: ' + rtaMapAtributes);
+
+const rtaMapFlat = users.flatMap(user => user.atributes);
+console.log('Respuesta con flatMap: ' + rtaMapFlat);
+
