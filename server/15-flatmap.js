6cbd69589884d8a81bfd348f5979f4e6ec452c11
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-18 16:03:28 -0500

    Create 15-flatmap.js

---
+++ server/15-flatmap.js
@@ -0,0 +1,5 @@
+const users = [
+    {userID : 1 , NameUser: 'Diego' , atributes : [25, 'Frontend', 'Rappi']},
+    {userID : 2 , NameUser: 'Fernando' , atributes : [28, 'Analista', 'IBM']},
+    {userID : 3 , NameUser: 'José' , atributes : [29, 'Data Scientist', 'Oracle']},
+];
