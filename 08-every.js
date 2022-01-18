a1c5db2be9d246cb86ce5d74ef082929ba93eb67
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-18 14:22:04 -0500

    Se han importado para el array un date como una librería y, se ha estudiado el algoritmo every.

---
+++ 08-every.js
@@ -0,0 +1,10 @@
+//! Contra-elemento de some y, se usa para decir qué todo y cada uno de los elementos cumple con una condición.
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+let rta = true ;
+
+for (let index = 0 ; index < numbers.length ; index ++ ) {
+    const element = element[index];
+}
--- server/07-someDates.js
+++ server/07-someDates.js
@@ -1,2 +1,35 @@
 //! Algoritmo para ver si se cruzan las fechas.
 
+const dates = [
+    {
+    startDate: new Date(2021, 1, 1, 10),
+    endDate: new Date(2021, 1, 1, 11),
+    title: "Cita de trabajo",
+    },
+    {
+    startDate: new Date(2021, 1, 1, 15),
+    endDate: new Date(2021, 1, 1, 15, 30),
+    title: "Cita con mi jefe",
+    },
+    {
+    startDate: new Date(2021, 1, 1, 20),
+    endDate: new Date(2021, 1, 1, 21),
+    title: "Cena",
+    },
+];
+const newAppiinment = {
+    startDate: new Date(2021,1,1,19),
+    endDate: new Date(2021,1,1,20,30),
+};
+const areIntervalsOverlapping = require('date-fns/areIntervalsOverlapping');
+
+const isOverLap = (newDate) => {
+    return dates.some(date => {
+        return areIntervalsOverlapping (
+            {start: date.startDate , end: date.endDate},
+            {start: newDate.startDate , end: newData.endDate},
+        )
+    })
+}
+
+console.log(isOverLap(newAppiinment));
