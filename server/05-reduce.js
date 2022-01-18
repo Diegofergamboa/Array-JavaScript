8062b013c4c9aff8659cdcec4a6bae71ca422152
Diego Fernando Ortiz Gamboa <72028938+Diegofergamboa@users.noreply.github.com>
2022-01-18 13:45:15 -0500

    Update 05-reduce.js

--- server/05-reduce.js
+++ server/05-reduce.js
@@ -14,3 +14,17 @@ console.log('respuesta con reduce method es: ' + sumOnReduce);
 
 //! Realizar un histograma con reduce.
 
+const numbers =  [1,2,1,2,1,2,3,4,3,5,10];
+
+const histograma = numbers.reduce((objeto,num)=>{
+    if (!objeto[num]) {
+        objeto[num] = 1 ;
+    } else {
+        objeto[num] += 1 ;
+        }
+    return objeto;
+},{});
+
+console.log(histograma);
+
+
